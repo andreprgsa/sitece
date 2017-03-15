f699ba0d4c5e5e340228e666cb16f584c9c645ad
Coraline Wodling <coraline.wodling@wavestone.com>
2017-03-15 14:10:24 +0100

    Evènements plats

--- src/app/pages/events/mock-events.ts
+++ src/app/pages/events/mock-events.ts
@@ -1,5 +1,5 @@
 import { Event } from './event';
 export var EVENTS: Event[] = [
   { id: 1, eligibility:'b', title: 'Massages assis', description: 'Le <b>CE WAVESTONE SA</b> vous invite à venir profiter d’une nouvelle session de « Massage Assis » dans une ambiance ZEN. <br /> La salle FITNESS, aménagée pour l’occasion, vous accueillera pour une session d’Amma assis de 20 mn.', cost:0, contactUrl: "cedric.ayme@wavestone.com", imgUrl:"http://maxpixel.freegreatpicture.com/static/photo/1x/Relaxing-Relaxation-Massage-Wellness-Relax-Spa-589775.jpg", place:"Tour Franklin", date:"27 mars 2017", sponsor:"Entreprise partenaire", eventUrl:"google.fr"},
-  { id: 2, eligibility:'bo', title: 'Boudoir des Cocottes', description: 'TBD', cost:0, contactUrl: "abdelghafour.benkassmi@wavestone.com", imgUrl:"https://upload.wikimedia.org/wikipedia/commons/f/ff/Red_nail_polish_in_application.jpg", place:"Tour Franklin", date:"27 mars 2017", sponsor:"Entreprise partenaire", eventUrl:"google.fr"}
+  { id: 2, eligibility:'bo', title: 'Boudoir des Cocottes', description: 'TBD', cost:5, contactUrl: "abdelghafour.benkassmi@wavestone.com", imgUrl:"https://upload.wikimedia.org/wikipedia/commons/f/ff/Red_nail_polish_in_application.jpg", place:"Tour Franklin", date:"27 mars 2017", sponsor:"Entreprise partenaire", eventUrl:"google.fr"}
 ];
