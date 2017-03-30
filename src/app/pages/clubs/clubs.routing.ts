927ed8ee1f95918d9b45dafe34e1feae7aaaf434
Coraline Wodling <coraline.wodling@wavestone.com>
2017-03-30 16:08:08 +0200

    Pages - il faut supprimer les trucs linemaps

--- src/app/pages/clubs/clubs.routing.ts
+++ src/app/pages/clubs/clubs.routing.ts
@@ -6,6 +6,11 @@ import { OverviewClubs } from './components/overviewClubs/overviewClubs.componen
 import { SoccerClubs } from './components/soccerClubs/soccerClubs.component';
 import { BasketClubs } from './components/basketClubs/basketClubs.component';
 import { HandClubs } from './components/handClubs/handClubs.component';
+import { RunningClubs } from './components/runningClubs/runningClubs.component';
+import { BadmintonClubs } from './components/badmintonClubs/badmintonClubs.component';
+import { GrimpClubs } from './components/grimpClubs/grimpClubs.component';
+import { TennisClubs } from './components/tennisClubs/tennisClubs.component';
+import { MusicClubs } from './components/musicClubs/musicClubs.component';
 
 // noinspection TypeScriptValidateTypes
 const routes: Routes = [
@@ -17,7 +22,12 @@ const routes: Routes = [
       { path: 'overviewClubs', component: OverviewClubs },
       { path: 'soccerClubs', component: SoccerClubs },
       { path: 'basketClubs', component: BasketClubs },
-      { path: 'handClubs', component: HandClubs }
+      { path: 'handClubs', component: HandClubs },
+      { path: 'runningClubs', component: RunningClubs },
+      { path: 'badmintonClubs', component: BadmintonClubs },
+      { path: 'grimpClubs', component: GrimpClubs },
+      { path: 'tennisClubs', component: TennisClubs },
+      { path: 'musicClubs', component: MusicClubs }
     ]
   }
 ];
