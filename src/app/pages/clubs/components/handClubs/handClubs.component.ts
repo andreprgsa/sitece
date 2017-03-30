7bf2c2f6c3a249f98edb60773a1477e5da5b14d2
Coraline Wodling <coraline.wodling@wavestone.com>
2017-03-30 14:56:38 +0200

    Club hand

--- src/app/pages/clubs/components/handClubs/handClubs.component.ts
+++ src/app/pages/clubs/components/handClubs/handClubs.component.ts
@@ -1,13 +1,26 @@
-import {Component, ElementRef} from '@angular/core';
+import {Component, OnInit, ElementRef} from '@angular/core';
+import {Club} from '../../club';
+import {ClubService} from '../../club.service';
 
 @Component({
-  selector: 'hand-clubs',
-  templateUrl: './handClubs.html'
+  selector: 'hand-clubs', //change selon le club
+  providers: [ClubService],
+  templateUrl: './handClubs.html' //change selon le club
 })
 
-export class HandClubs{
+export class HandClubs{ //change selon le club
 
-  constructor() {
+  clubs: Club[];
+  selectedClub: Club;
+
+  constructor(private clubService: ClubService) {}
+
+  getMyClub(): void {
+    this.clubService.getClubs().then(clubs => this.clubs = clubs);
+  }
+
+  ngOnInit(): void {
+    this.getMyClub();
   }
 
 }
